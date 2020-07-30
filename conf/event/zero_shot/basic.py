1b0776b0b4f36a61551389879a25a547883c948d
Zaid Sheikh <zsheikh@cs.cmu.edu>
2020-07-30 15:44:00 -0400

    switch to M36 ontology

--- conf/event/zero_shot/basic.py
+++ conf/event/zero_shot/basic.py
@@ -21,7 +21,7 @@ c.ZeroShotEventResources.word_embedding_path = os.path.join(
 c.ZeroShotEventResources.word_vocab_path = os.path.join(
     base, 'event_embeddings', 'word_embeddings.voc')
 c.ZeroShotEventResources.target_ontology = os.path.join(
-    csr_resources, 'LDCOntology_v0.1.jsonld')
+    csr_resources, 'LDCOntologyM36.jsonld')
 
 c.Basic.input_path = '/home/hector/workspace/aida/test_docs/hector_out/english/rich/simple_run'
 c.Basic.output_path = '/home/hector/workspace/aida/test_docs/hector_out/rich/enlish/simple_run_mapped'
